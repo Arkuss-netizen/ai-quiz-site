a94a1df4f06b0157cce6eaee24a43fde80b735a2
Arkuss-netizen <markuss2101@icloud.com>
2025-01-20 11:27:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ document.getElementById("quiz-form").addEventListener("submit", function(event) 
         q4: document.querySelector('input[name="q4"]:checked')?.value,
         q5: document.querySelector('input[name="q5"]:checked')?.value,
         q6: document.querySelector('input[name="q6"]:checked')?.value,
-        q6: document.querySelector('input[name="q7"]:checked')?.value,
-        q6: document.querySelector('input[name="q8"]:checked')?.value,
-        q6: document.querySelector('input[name="q9"]:checked')?.value
+        q7: document.querySelector('input[name="q7"]:checked')?.value,
+        q8: document.querySelector('input[name="q8"]:checked')?.value,
+        q9: document.querySelector('input[name="q9"]:checked')?.value
     };
 
     // Validate answers
